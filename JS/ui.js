20e9e859caa099ca01d6e9a67cb803c700ea3e82
Hiwotgebre <hiwotg173@gmail.com>
2024-05-26 21:22:29 -0400

    adding some functionality for User interface manipulation

--- JS/ui.js
+++ JS/ui.js
@@ -1,22 +1,10 @@
 function displayImages(images) {
-    console.log(images); //check what the API returns
     const gallery = document.getElementById('gallery');
     gallery.innerHTML = images.map(image => `
-    <div class="col-md-4 mb-3">
-    <div class="card">
-        <img src="${image.url}" class="card-img-top" alt="Cat" onclick="openLightbox('${image.url}')">
-    </div>
-</div>
+        <div class="col-4 mb-4">
+            <div class="card">
+                <img src="${image.url}" class="card-img-top" alt="Animal">
+            </div>
+        </div>
     `).join('');
-
-}
-  
-function openLightbox(src) {
-    const lightboxImage = document.getElementById('lightbox-image');
-    lightboxImage.src = src;
-    document.getElementById('lightbox-modal').style.display = 'flex';
-}
-
-function closeLightbox() {
-    document.getElementById('lightbox-modal').style.display = 'none';
 }
